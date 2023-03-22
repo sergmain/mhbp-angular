9f6f8d8e237a3185d346f327ea4ef3c8e483a4b4
sergmain <serg_main@yahoo.com>
2023-03-21 21:30:03 -0700

    .

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { AuthGuard } from '@app/guards/auth/auth.guard';
-import { BillingComponent } from './components/billing/billing.component';
-import { PilotComponent } from './components/pilot/pilot.component';
 import { environment } from '@src/environments/environment';
 import { AppIndexComponent } from './components/app-index/app-index.component';
 const routes: Routes = [
@@ -16,21 +14,6 @@ const routes: Routes = [
         loadChildren: () => import('src/app/modules/dispatcher/dispatcher.module').then(m => m.DispatcherModule),
         data: { sidenavExist: true }
     },
-    {
-        path: 'billing',
-        canActivate: [AuthGuard],
-        component: BillingComponent,
-    },
-    {
-        path: 'pilot',
-        component: PilotComponent,
-    },
-    {
-        path: 'ai',
-        loadChildren: () => import('src/app/modules/ai/ai.module').then(m => m.AiModule),
-        data: { sidenavExist: true }
-    },
-
     {
         path: 'batch',
         loadChildren: () => import('src/app/modules/batch/batch.module').then(m => m.BatchModule),
